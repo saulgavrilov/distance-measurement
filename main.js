0daebc25950adf33f42686ab7d0acb5bb399069c
Saul Gavrilov <48588501+saulgavrilov@users.noreply.github.com>
2022-08-05 11:07:25 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+navigator.mediaDevices
+  .getDisplayMedia({
+    video: {
+      facingMode: 'environment',
+    },
+  })
+  .then((signal) => {
+    const video = document.querySelector('.video');
+    video.src = signal;
+    video.play();
+  })
+  .catch((e) => {
+    console.log(e);
+    return;
+  });
+
+window.addEventListener('deviceorientation', (e) => {
+  let angle = e.beta - 90;
+
+  if (angle < 0) {
+    angle = 0;
+  }
+
+  const distance = 22;
+  const height = Math.tan((angle * Math.PI) / 180) * distance;
+
+  document.querySelector('.height').innerText = `${height.toFixed(1)}m`;
+  document.querySelector('.degree').innerText = `${height.toFixed(1)}m`;
+
+  console.log(angle);
+});
